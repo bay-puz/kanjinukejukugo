00d1e19e82c889a96e0d10029653f5d608e807cc
bay <68962000+bay-puz@users.noreply.github.com>
2026-08-17 06:28:47 +0900

    解析の仕方をきれいにする

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -5,11 +5,18 @@ function showAnalytics(inputList) {
         countDict[char] = (countDict[char] || 0) + 1
     }
 
-    showCount(analizeCount(allKanji, countDict))
-    showIncluded(analizeIncluded(allKanji, countDict, inputList))
+    showCount(inputList)
+    showUsed(inputList)
 }
 
-function analizeCount(allKanji, countDict) {
+
+function showCount(inputList) {
+    var allKanji = inputList.join('')
+    var countDict = {}
+    for (const char of allKanji) {
+        countDict[char] = (countDict[char] || 0) + 1
+    }
+
     var countKanji = {}
     for (const [kanji, count] of Object.entries(countDict)) {
         if ( countKanji[count] === undefined ) {
@@ -17,29 +24,33 @@ function analizeCount(allKanji, countDict) {
         }
         countKanji[count].push(kanji)
     }
-    return countKanji
-}
 
-function showCount(countKanji) {
     if (countKanji.length <= 0) {
         return
     }
     var countElement = document.getElementById("countKanji")
     countElement.innerHTML = null
-    for (const [kanji, count] of Object.entries(countKanji)) {
+    for (const [count, kanji] of Object.entries(countKanji)) {
         const element = document.createElement("p")
-        element.innerText = `${kanji}: ${count}`
+        element.innerText = count + "回：" + kanji.join("、") + "（" + kanji.length + "個）"
         countElement.appendChild(element)
     }
 }
 
-function analizeIncluded(allKanji, countDict, inputList) {
+function showUsed(inputList) {
+    var allKanji = inputList.join('')
+    var countDict = {}
+    for (const char of allKanji) {
+        countDict[char] = (countDict[char] || 0) + 1
+    }
+
     var nukeSet = new Set()
     for (const char of allKanji) {
         if (countDict[char] > 1) {
             nukeSet.add(char)
         }
     }
+
     var jukugoNuke = []
     for (const line of inputList) {
         var nukeKanji = new Set()
@@ -50,6 +61,7 @@ function analizeIncluded(allKanji, countDict, inputList) {
         }
         jukugoNuke.push(nukeKanji)
     }
+    var resultList = []
     var includedJukugo = []
     var sameJukugo = []
     for (const [i, nuke1] of jukugoNuke.entries()) {
@@ -63,38 +75,56 @@ function analizeIncluded(allKanji, countDict, inputList) {
             if (nuke2.size === 0) {
                 continue
             }
-            if (nuke1.isSubsetOf(nuke2)) {
-                if (nuke1.size === nuke2.size) {
-                    if (i < j) {
-                        sameJukugo.push([inputList[i], inputList[j]])
-                    }
+            if (!nuke1.isSubsetOf(nuke2)) {
+                continue
+            }
+
+            if (nuke1.size === nuke2.size) {
+                if (i < j) {
+                    resultList.push(sameAlert(inputList[i], inputList[j]))
                 }
-                else {
-                    includedJukugo.push([inputList[i], inputList[j]])
-                }
+            }
+            else {
+                resultList.push(includedAlert(inputList[i], inputList[j]))
             }
         }
     }
-    return [includedJukugo, sameJukugo]
-}
 
-function showIncluded(includedList) {
-    const includedJukugo = includedList[0]
-    const sameJukugo = includedList[1]
-    if (includedJukugo.length <= 0 && sameJukugo.length <= 0) {
+    for (const jukugo of inputList) {
+        var usingDict = {}
+        for (const kanji of jukugo) {
+            usingDict[kanji] = (usingDict[kanji] || 0) + 1
+        }
+        for (const [kanji, count] of Object.entries(usingDict)) {
+            if (count < 2) {
+                continue
+            }
+            if (count === countDict[kanji]){
+                resultList.push(aloneAlert(kanji, jukugo))
+            }
+        }
+    }
+
+    if (resultList.length === 0) {
         return
     }
-    var includedElement = document.getElementById("includedJukugo")
-    includedElement.innerHTML = null
-    for (const [jukugo1, jukugo2] of includedJukugo) {
+    var usedElement = document.getElementById("usedKanji")
+    usedElement.innerHTML = null
+    for (const msg of resultList) {
         const element = document.createElement("p")
-        element.innerText = jukugo1 + " は " + jukugo2 + " に含まれる"
-        includedElement.appendChild(element)
+        element.innerText = msg
+        usedElement.appendChild(element)
     }
-    for (const [jukugo1, jukugo2] of sameJukugo) {
-        const element = document.createElement("p")
-        element.innerText = jukugo1 + " と " + jukugo2 + " は同じ"
-        includedElement.appendChild(element)
-    }
+}
 
+function sameAlert(j1, j2) {
+    return j1 + "と" + j2 + "は抜けている漢字が同じ"
+}
+
+function includedAlert(j1, j2) {
+    return j1 + "の抜けている漢字は" + j2 + "に含まれる"
+}
+
+function aloneAlert(k, j) {
+    return "「" + k + "」は" + j + "でしか使われていない"
 }
